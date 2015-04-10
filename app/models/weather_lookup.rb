5a7012ebcfc75eb0eede7e573fe113893add3080
christopherhuii <christopher.kam.hui@gmail.com>
2015-04-09 17:45:36 -0700

    error handling for bad addresses added

--- app/models/weather_lookup.rb
+++ app/models/weather_lookup.rb
@@ -1,5 +1,5 @@
 class WeatherLookup
-  attr_accessor :temperature, :weather_condition, :city, :state, :zip
+  attr_accessor :temperature, :weather_condition, :city, :state, :zip, :error
 
   def initialize(zip)
     self.zip = zip
@@ -13,9 +13,13 @@ class WeatherLookup
   end
 
   def weather_values(weather_hash)
-    self.temperature = weather_hash.parsed_response['current_observation']['temp_f']
-    self.weather_condition = weather_hash.parsed_response['current_observation']['weather']
-    self.city = weather_hash.parsed_response['location']['city']
-    self.state = weather_hash.parsed_response['location']['state']
+    if weather_hash.parsed_response['response']['error'].blank?
+      self.temperature = weather_hash.parsed_response['current_observation']['temp_f']
+      self.weather_condition = weather_hash.parsed_response['current_observation']['weather']
+      self.city = weather_hash.parsed_response['location']['city']
+      self.state = weather_hash.parsed_response['location']['state']
+    else
+      self.error = "No Cities Match Your Search Query"
+    end
   end
 end
